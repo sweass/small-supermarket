90a401eaa949e53dca0287d6e172770c2ad31c46
Hikari <hikari@all.cn>
2023-08-02 10:58:03 +0800

    初始化项目部署

---
+++ src/main/java/com/szb/SmallSupermarketApplication.java
@@ -0,0 +1,13 @@
+package com.szb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallSupermarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallSupermarketApplication.class, args);
+    }
+
+}
